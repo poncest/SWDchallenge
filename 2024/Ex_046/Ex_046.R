50b8f66238d02e48e432a6ac70b4bbcbe5a56881
poncest <steven_ponce@yahoo.com>
2024-10-11 10:32:07 -0400

    Exercise 4.6

--- 2024/Ex_046/Ex_046.R
+++ 2024/Ex_046/Ex_046.R
@@ -75,14 +75,14 @@ sales_table <- sales_data |>
       )
     }
   ) |>
-  # Emphasize Account H
+  # Emphasize Accounts H and J
   tab_style(
     style = list(
       cell_fill(color = "#D3E4CD"),
       cell_text(color = "black")
     ),
     locations = cells_body(
-      rows = account == "H" & percent_change_vs_prior == max(percent_change_vs_prior)
+      rows = account %in% c("J","H")
     )
   ) |>
   # Now add the color gradient for the % Change column
@@ -113,7 +113,7 @@ sales_table <- sales_data |>
   tab_source_note(
     source_note = html("<span style='font-size: 10px;'>UPC is the Universal Product Code, the barcode symbology.<br>ACV is All-Commodity Volume, measured as a percentage from 0 to 100.</span>")
   ) |>
-  ttab_footnote(
+  tab_footnote(
     footnote = md("**Account H shows the highest positive growth (+37.90%)**, **Account D has the highest sales volume ($547,265)**, **Account J shows the largest decrease (-8.70%)**</span>"),
     locations = cells_title(groups = "title")
   ) |>
@@ -155,9 +155,9 @@ gtsave(data = sales_table,
        )
 
 # 5. SESSION INFO ----  
-sessioninfo::session_info(include_base = TRUE) 
+sessioninfo::session_info(include_base = FALSE) 
  
-# ─ Session info ───────────────────────────────────────────────────────────────────────────────────
+# ─ Session info ────────────────────────────────────────────────────────────────────────────────
 # setting  value
 # version  R version 4.4.1 (2024-06-14 ucrt)
 # os       Windows 10 x64 (build 19045)
@@ -167,103 +167,85 @@ sessioninfo::session_info(include_base = TRUE)
 # collate  English_United States.utf8
 # ctype    English_United States.utf8
 # tz       America/New_York
-# date     2024-10-09
+# date     2024-10-11
 # rstudio  2024.09.0+375 Cranberry Hibiscus (desktop)
 # pandoc   NA
 # 
-# ─ Packages ───────────────────────────────────────────────────────────────────────────────────────
-# ! package     * version  date (UTC) lib source
-# V base        * 4.4.1    2024-04-24 [2] local (on disk 4.4.0)
-# P base64enc     0.1-3    2015-07-28 [?] CRAN (R 4.4.0)
-# P camcorder     0.1.0    2022-10-03 [?] CRAN (R 4.4.0)
-# P cellranger    1.1.0    2016-07-27 [?] CRAN (R 4.4.0)
-# chromote      0.3.1    2024-08-30 [1] CRAN (R 4.4.1)
-# P cli           3.6.3    2024-06-21 [?] CRAN (R 4.4.1)
-# colorspace    2.1-0    2023-01-23 [1] CRAN (R 4.4.0)
-# P commonmark    1.9.1    2024-01-30 [?] CRAN (R 4.4.0)
-# P compiler      4.4.0    2024-04-24 [?] local
-# P datasets    * 4.4.0    2024-04-24 [?] local
-# P digest        0.6.35   2024-03-11 [?] CRAN (R 4.4.0)
-# dplyr       * 1.1.4    2023-11-17 [1] CRAN (R 4.4.0)
-# P fansi         1.0.6    2023-12-08 [?] CRAN (R 4.4.0)
-# farver        2.1.1    2022-07-06 [1] CRAN (R 4.4.0)
-# P fastmap       1.1.1    2023-02-24 [?] CRAN (R 4.4.0)
-# forcats     * 1.0.0    2023-01-29 [1] CRAN (R 4.4.0)
-# P fs            1.6.4    2024-04-25 [?] CRAN (R 4.4.0)
-# generics      0.1.3    2022-07-05 [1] CRAN (R 4.4.0)
-# ggplot2     * 3.5.1    2024-04-23 [1] CRAN (R 4.4.0)
-# P ggtext      * 0.1.2    2022-09-16 [?] CRAN (R 4.4.0)
-# P gifski        1.12.0-2 2023-08-12 [?] CRAN (R 4.4.0)
-# P glue        * 1.8.0    2024-09-30 [?] CRAN (R 4.4.1)
-# P graphics    * 4.4.0    2024-04-24 [?] local
-# P grDevices   * 4.4.0    2024-04-24 [?] local
-# P grid          4.4.0    2024-04-24 [?] local
-# P gridtext      0.1.5    2022-09-16 [?] CRAN (R 4.4.0)
-# P gt          * 0.11.1   2024-10-04 [?] CRAN (R 4.4.1)
-# gtable        0.3.5    2024-04-22 [1] CRAN (R 4.4.0)
-# P here          1.0.1    2020-12-13 [?] CRAN (R 4.4.0)
-# P hms           1.1.3    2023-03-21 [?] CRAN (R 4.4.0)
-# P htmltools     0.5.8.1  2024-04-04 [?] CRAN (R 4.4.0)
-# P janitor     * 2.2.0    2023-02-02 [?] CRAN (R 4.4.0)
-# P jsonlite      1.8.8    2023-12-04 [?] CRAN (R 4.4.0)
-# P knitr         1.46     2024-04-06 [?] CRAN (R 4.4.0)
-# P later         1.3.2    2023-12-06 [?] CRAN (R 4.4.0)
-# P lifecycle     1.0.4    2023-11-07 [?] CRAN (R 4.4.0)
-# P lubridate   * 1.9.3    2023-09-27 [?] CRAN (R 4.4.0)
-# magick        2.8.3    2024-02-18 [1] CRAN (R 4.4.0)
-# P magrittr      2.0.3    2022-03-30 [?] CRAN (R 4.4.0)
-# P markdown      1.13     2024-06-04 [?] CRAN (R 4.4.1)
-# P methods     * 4.4.0    2024-04-24 [?] local
-# munsell       0.5.1    2024-04-01 [1] CRAN (R 4.4.0)
-# P pacman        0.5.1    2019-03-11 [?] CRAN (R 4.4.0)
-# P pillar        1.9.0    2023-03-22 [?] CRAN (R 4.4.0)
-# P pkgconfig     2.0.3    2019-09-22 [?] CRAN (R 4.4.0)
-# P processx      3.8.4    2024-03-16 [?] CRAN (R 4.4.0)
-# P promises      1.3.0    2024-04-05 [?] CRAN (R 4.4.0)
-# P ps            1.7.6    2024-01-18 [?] CRAN (R 4.4.0)
-# P purrr       * 1.0.2    2023-08-10 [?] CRAN (R 4.4.0)
-# P R6            2.5.1    2021-08-19 [?] CRAN (R 4.4.0)
-# P Rcpp          1.0.12   2024-01-09 [?] CRAN (R 4.4.0)
-# P readr       * 2.1.5    2024-01-10 [?] CRAN (R 4.4.0)
-# P readxl      * 1.4.3    2023-07-06 [?] CRAN (R 4.4.0)
-# P rematch       2.0.0    2023-08-30 [?] CRAN (R 4.4.0)
-# renv          1.0.7    2024-04-11 [1] CRAN (R 4.4.0)
-# P repr          1.1.7    2024-03-22 [?] CRAN (R 4.4.0)
-# P rlang         1.1.4    2024-06-04 [?] CRAN (R 4.4.1)
-# P rprojroot     2.0.4    2023-11-05 [?] CRAN (R 4.4.0)
-# P rstudioapi    0.16.0   2024-03-24 [?] CRAN (R 4.4.0)
-# P rsvg          2.6.0    2023-10-08 [?] CRAN (R 4.4.0)
-# P sass          0.4.9    2024-03-15 [?] CRAN (R 4.4.0)
-# scales      * 1.3.0    2023-11-28 [1] CRAN (R 4.4.0)
-# P sessioninfo   1.2.2    2021-12-06 [?] CRAN (R 4.4.0)
-# P showtext    * 0.9-7    2024-03-02 [?] CRAN (R 4.4.0)
-# P showtextdb  * 3.0      2020-06-04 [?] CRAN (R 4.4.0)
-# P skimr       * 2.1.5    2022-12-23 [?] CRAN (R 4.4.0)
-# P snakecase     0.11.1   2023-08-27 [?] CRAN (R 4.4.0)
-# P stats       * 4.4.0    2024-04-24 [?] local
-# P stringi       1.8.3    2023-12-11 [?] CRAN (R 4.4.0)
-# P stringr     * 1.5.1    2023-11-14 [?] CRAN (R 4.4.0)
-# P svglite       2.1.3    2023-12-08 [?] CRAN (R 4.4.0)
-# P sysfonts    * 0.8.9    2024-03-02 [?] CRAN (R 4.4.0)
-# P systemfonts   1.1.0    2024-05-15 [?] CRAN (R 4.4.0)
-# P tibble      * 3.2.1    2023-03-20 [?] CRAN (R 4.4.0)
-# tidyr       * 1.3.1    2024-01-24 [1] CRAN (R 4.4.0)
-# tidyselect    1.2.1    2024-03-11 [1] CRAN (R 4.4.0)
-# P tidyverse   * 2.0.0    2023-02-22 [?] CRAN (R 4.4.0)
-# P timechange    0.3.0    2024-01-18 [?] CRAN (R 4.4.0)
-# P tools         4.4.0    2024-04-24 [?] local
-# P tzdb          0.4.0    2023-05-12 [?] CRAN (R 4.4.0)
-# P utf8          1.2.4    2023-10-22 [?] CRAN (R 4.4.0)
-# P utils       * 4.4.0    2024-04-24 [?] local
-# P vctrs         0.6.5    2023-12-01 [?] CRAN (R 4.4.0)
-# webshot2      0.1.1    2023-08-11 [1] CRAN (R 4.4.1)
-# websocket     1.4.2    2024-07-22 [1] CRAN (R 4.4.1)
-# P withr         3.0.0    2024-01-16 [?] CRAN (R 4.4.0)
-# P xfun          0.43     2024-03-25 [?] CRAN (R 4.4.0)
-# P xml2          1.3.6    2023-12-04 [?] CRAN (R 4.4.0)
+# ─ Packages ────────────────────────────────────────────────────────────────────────────────────
+# ! package     * version date (UTC) lib source
+# P base64enc     0.1-3   2015-07-28 [?] CRAN (R 4.4.0)
+# P cellranger    1.1.0   2016-07-27 [?] CRAN (R 4.4.0)
+# chromote      0.3.1   2024-08-30 [1] CRAN (R 4.4.1)
+# P cli           3.6.3   2024-06-21 [?] CRAN (R 4.4.1)
+# colorspace    2.1-0   2023-01-23 [1] CRAN (R 4.4.0)
+# P commonmark    1.9.1   2024-01-30 [?] CRAN (R 4.4.0)
+# P digest        0.6.35  2024-03-11 [?] CRAN (R 4.4.0)
+# dplyr       * 1.1.4   2023-11-17 [1] CRAN (R 4.4.0)
+# P fansi         1.0.6   2023-12-08 [?] CRAN (R 4.4.0)
+# farver        2.1.1   2022-07-06 [1] CRAN (R 4.4.0)
+# P fastmap       1.1.1   2023-02-24 [?] CRAN (R 4.4.0)
+# forcats     * 1.0.0   2023-01-29 [1] CRAN (R 4.4.0)
+# P fs            1.6.4   2024-04-25 [?] CRAN (R 4.4.0)
+# generics      0.1.3   2022-07-05 [1] CRAN (R 4.4.0)
+# ggplot2     * 3.5.1   2024-04-23 [1] CRAN (R 4.4.0)
+# P ggtext      * 0.1.2   2022-09-16 [?] CRAN (R 4.4.0)
+# P glue        * 1.8.0   2024-09-30 [?] CRAN (R 4.4.1)
+# P gridtext      0.1.5   2022-09-16 [?] CRAN (R 4.4.0)
+# P gt          * 0.11.1  2024-10-04 [?] CRAN (R 4.4.1)
+# gtable        0.3.5   2024-04-22 [1] CRAN (R 4.4.0)
+# P hms           1.1.3   2023-03-21 [?] CRAN (R 4.4.0)
+# P htmltools     0.5.8.1 2024-04-04 [?] CRAN (R 4.4.0)
+# P janitor     * 2.2.0   2023-02-02 [?] CRAN (R 4.4.0)
+# P jsonlite      1.8.8   2023-12-04 [?] CRAN (R 4.4.0)
+# P knitr         1.46    2024-04-06 [?] CRAN (R 4.4.0)
+# P later         1.3.2   2023-12-06 [?] CRAN (R 4.4.0)
+# P lifecycle     1.0.4   2023-11-07 [?] CRAN (R 4.4.0)
+# P lubridate   * 1.9.3   2023-09-27 [?] CRAN (R 4.4.0)
+# P magrittr      2.0.3   2022-03-30 [?] CRAN (R 4.4.0)
+# P markdown      1.13    2024-06-04 [?] CRAN (R 4.4.1)
+# munsell       0.5.1   2024-04-01 [1] CRAN (R 4.4.0)
+# P pacman        0.5.1   2019-03-11 [?] CRAN (R 4.4.0)
+# P pillar        1.9.0   2023-03-22 [?] CRAN (R 4.4.0)
+# P pkgconfig     2.0.3   2019-09-22 [?] CRAN (R 4.4.0)
+# P processx      3.8.4   2024-03-16 [?] CRAN (R 4.4.0)
+# P promises      1.3.0   2024-04-05 [?] CRAN (R 4.4.0)
+# P ps            1.7.6   2024-01-18 [?] CRAN (R 4.4.0)
+# P purrr       * 1.0.2   2023-08-10 [?] CRAN (R 4.4.0)
+# P R6            2.5.1   2021-08-19 [?] CRAN (R 4.4.0)
+# P Rcpp          1.0.12  2024-01-09 [?] CRAN (R 4.4.0)
+# P readr       * 2.1.5   2024-01-10 [?] CRAN (R 4.4.0)
+# P readxl      * 1.4.3   2023-07-06 [?] CRAN (R 4.4.0)
+# P rematch       2.0.0   2023-08-30 [?] CRAN (R 4.4.0)
+# renv          1.0.7   2024-04-11 [1] CRAN (R 4.4.0)
+# P repr          1.1.7   2024-03-22 [?] CRAN (R 4.4.0)
+# P rlang         1.1.4   2024-06-04 [?] CRAN (R 4.4.1)
+# P rstudioapi    0.16.0  2024-03-24 [?] CRAN (R 4.4.0)
+# P sass          0.4.9   2024-03-15 [?] CRAN (R 4.4.0)
+# scales      * 1.3.0   2023-11-28 [1] CRAN (R 4.4.0)
+# P sessioninfo   1.2.2   2021-12-06 [?] CRAN (R 4.4.0)
+# P showtext    * 0.9-7   2024-03-02 [?] CRAN (R 4.4.0)
+# P showtextdb  * 3.0     2020-06-04 [?] CRAN (R 4.4.0)
+# P skimr       * 2.1.5   2022-12-23 [?] CRAN (R 4.4.0)
+# P snakecase     0.11.1  2023-08-27 [?] CRAN (R 4.4.0)
+# P stringi       1.8.3   2023-12-11 [?] CRAN (R 4.4.0)
+# P stringr     * 1.5.1   2023-11-14 [?] CRAN (R 4.4.0)
+# P sysfonts    * 0.8.9   2024-03-02 [?] CRAN (R 4.4.0)
+# P tibble      * 3.2.1   2023-03-20 [?] CRAN (R 4.4.0)
+# tidyr       * 1.3.1   2024-01-24 [1] CRAN (R 4.4.0)
+# tidyselect    1.2.1   2024-03-11 [1] CRAN (R 4.4.0)
+# P tidyverse   * 2.0.0   2023-02-22 [?] CRAN (R 4.4.0)
+# P timechange    0.3.0   2024-01-18 [?] CRAN (R 4.4.0)
+# P tzdb          0.4.0   2023-05-12 [?] CRAN (R 4.4.0)
+# P utf8          1.2.4   2023-10-22 [?] CRAN (R 4.4.0)
+# P vctrs         0.6.5   2023-12-01 [?] CRAN (R 4.4.0)
+# webshot2      0.1.1   2023-08-11 [1] CRAN (R 4.4.1)
+# websocket     1.4.2   2024-07-22 [1] CRAN (R 4.4.1)
+# P withr         3.0.0   2024-01-16 [?] CRAN (R 4.4.0)
+# P xfun          0.43    2024-03-25 [?] CRAN (R 4.4.0)
+# P xml2          1.3.6   2023-12-04 [?] CRAN (R 4.4.0)
 # 
-# V ── Loaded and on-disk version mismatch.
+# 
 # P ── Loaded and on-disk path mismatch.
 # 
-# ──────────────────────────────────────────────────────────────────────────────────────────────────
+# ───────────────────────────────────────────────────────────────────────────────────────────────
 # > 
